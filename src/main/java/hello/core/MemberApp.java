0c004982f7f934defbe286d7dafb0ce8ac39c098
gguksoon <mingsoo0807@gmail.com>
2021-06-21 21:25:50 +0900

    JAVA 코드 -> 스프링 코드로 전환

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,14 +3,16 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-
-        MemberService memberService = appConfig.memberService();
+        // AppConfig에 있는 환경설정 정보를 가지고 스프링이 스프링 컨테이너에 등록하여 관리하여 준다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
